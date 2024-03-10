6fa882891203481a17c4addc7c16941ffa6321a8
Tomi1819 <toshevtomi@gmail.com>
2024-03-10 14:05:35 +0200

    Repository pattern implemented

---
+++ AutoPartsWeb.Infrastructure/Data/Common/Repository.cs
@@ -0,0 +1,31 @@
+﻿namespace AutoPartsWeb.Infrastructure.Data.Common
+{
+    using AutoPartsWeb.Data;
+    using Microsoft.EntityFrameworkCore;
+
+    public class Repository : IRepository
+    {
+        private readonly DbContext context;
+
+        public Repository(AutoPartsWebDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IQueryable<T> All<T>() where T : class
+        {
+            return DbSet<T>();
+        }
+
+        public IQueryable<T> AllReadOnly<T>() where T : class
+        {
+            return DbSet<T>()
+                .AsNoTracking();
+        }
+
+        private DbSet<T> DbSet<T>() where T : class
+        {
+            return context.Set<T>();
+        }
+    }
+}
