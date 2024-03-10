3f7e7f64d26b07b58fde52d5372decdb8c1b8274
Tomi1819 <toshevtomi@gmail.com>
2024-03-11 00:24:18 +0200

    DecimalModelBinder added

---
+++ AutoPartsWeb/ModelBinders/DecimalModelBinder.cs
@@ -0,0 +1,43 @@
+﻿namespace AutoPartsWeb.ModelBinders
+{
+    using Microsoft.AspNetCore.Mvc.ModelBinding;
+    using System.Globalization;
+    using System.Threading.Tasks;
+
+    public class DecimalModelBinder : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            ValueProviderResult valueResult = bindingContext.ValueProvider
+                .GetValue(bindingContext.ModelName);
+
+            if (valueResult != ValueProviderResult.None &&
+                !string.IsNullOrEmpty(valueResult.FirstValue))
+            {
+                decimal result = 0m;
+                bool success = false;
+
+                try
+                {
+                    string strValue = valueResult.FirstValue.Trim();
+                    strValue = strValue.Replace(".", CultureInfo.InvariantCulture.NumberFormat.NumberDecimalSeparator);
+                    strValue = strValue.Replace(",", CultureInfo.InvariantCulture.NumberFormat.NumberDecimalSeparator);
+
+                    result = Convert.ToDecimal(strValue, CultureInfo.CurrentCulture);
+                    success = true;
+                }
+                catch (FormatException e)
+                {
+                    bindingContext.ModelState.AddModelError(bindingContext.ModelName, e, bindingContext.ModelMetadata);
+                }
+
+                if (success)
+                {
+                    bindingContext.Result = ModelBindingResult.Success(result);
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
