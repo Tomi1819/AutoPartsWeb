3fe733017a3e5e8718119534b5c35d3544fda621
Tomi1819 <toshevtomi@gmail.com>
2024-04-02 22:30:27 +0300

    Constants separated

--- AutoPartsWeb.Core/Constants/MessageConstants.cs
+++ AutoPartsWeb.Core/Constants/MessageConstants.cs
@@ -5,6 +5,9 @@
         public const string DateTimeFormat = "dd/MM/yyyy HH:mm";
 
         public const string DealerRequiredErrorMessage = "The {0} field is required";
+
         public const string DealerStringLenghtErrorMessage = "The {0} field must be {2} and {1} characters long";
+
+        public const string NotEligible = "Not eligible to become a dealer";
     }
 }
