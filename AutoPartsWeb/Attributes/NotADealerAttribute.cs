6fd3610779389fce244b3579c894b07e2817da3d
Tomi1819 <toshevtomi@gmail.com>
2024-04-02 20:50:59 +0300

    Custom attribute created

---
+++ AutoPartsWeb/Attributes/NotADealerAttribute.cs
@@ -0,0 +1,28 @@
+﻿namespace AutoPartsWeb.Attributes
+{
+    using AutoPartsWeb.Core.Contracts;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.Filters;
+    using System.Security.Claims;
+
+    public class NotADealerAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            base.OnActionExecuting(context);
+
+            IDealerService? dealerService = context.HttpContext.RequestServices.GetService<IDealerService>();
+
+            if (dealerService != null)
+            {
+                context.Result = new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+
+            if ( dealerService != null &&
+                dealerService.ExistsByIdAsync(context.HttpContext.User.Id()).Result)
+            {
+                context.Result = new StatusCodeResult(StatusCodes.Status400BadRequest);
+            }
+        }
+    }
+}
