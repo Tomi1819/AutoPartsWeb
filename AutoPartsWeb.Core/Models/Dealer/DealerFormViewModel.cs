49ddd11a9aee5a575f9a6d6025c4d9011e59a737
Tomi1819 <toshevtomi@gmail.com>
2024-04-13 17:42:46 +0300

    Constants renamed

--- AutoPartsWeb.Core/Models/Dealer/DealerFormViewModel.cs
+++ AutoPartsWeb.Core/Models/Dealer/DealerFormViewModel.cs
@@ -6,30 +6,30 @@
 
     public class DealerFormViewModel
     {
-        [Required(ErrorMessage = DealerRequiredErrorMessage)]
+        [Required(ErrorMessage = RequiredErrorMessage)]
         [StringLength(DealerMaxNameLength, 
             MinimumLength = DealerMinNameLength, 
-            ErrorMessage = DealerStringLenghtErrorMessage)]
+            ErrorMessage = StringLenghtErrorMessage)]
         public string Name { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = DealerRequiredErrorMessage)]
+        [Required(ErrorMessage = RequiredErrorMessage)]
         [StringLength(DealerCompanyNameMaxLength, 
             MinimumLength = DealerCompanyNameMinLength,
-            ErrorMessage = DealerStringLenghtErrorMessage)]
+            ErrorMessage = StringLenghtErrorMessage)]
         public string CompanyName { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = DealerRequiredErrorMessage)]
+        [Required(ErrorMessage = RequiredErrorMessage)]
         [StringLength(DealerCompanyNumberMaxLength,
             MinimumLength = DealerCompanyNumberMinLength,
-            ErrorMessage = DealerStringLenghtErrorMessage)]
+            ErrorMessage = StringLenghtErrorMessage)]
         public string CompanyNumber { get; set; } = string.Empty;
 
         [StringLength(DealerDescriptionMaxLength,
             MinimumLength = DealerDescriptionMinLength,
-            ErrorMessage = DealerStringLenghtErrorMessage)]
+            ErrorMessage = StringLenghtErrorMessage)]
         public string Description { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = DealerRequiredErrorMessage)]
+        [Required(ErrorMessage = RequiredErrorMessage)]
         public string UserId { get; set; } = string.Empty;
     }
 }
