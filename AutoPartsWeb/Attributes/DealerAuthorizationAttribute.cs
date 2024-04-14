63dfa67ef0e2c6a6cf5eda047bb1171a333c218a
Tomi1819 <toshevtomi@gmail.com>
2024-04-14 18:26:26 +0300

    New attribute created

---
+++ AutoPartsWeb/Attributes/DealerAuthorizationAttribute.cs
@@ -0,0 +1,18 @@
+﻿namespace AutoPartsWeb.Attributes
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.Filters;
+
+    public class DealerAuthorizationAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            base.OnActionExecuting(context);
+
+            if (!context.HttpContext.User.Identity.IsAuthenticated || !context.HttpContext.User.IsInRole("Dealer"))
+            {
+                context.Result = new StatusCodeResult(StatusCodes.Status403Forbidden);
+            }
+        }
+    }
+}
